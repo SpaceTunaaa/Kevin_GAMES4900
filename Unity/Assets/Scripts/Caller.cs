1553f6f953ee0595d1376e0f116b399010021899
SpaceTunaaa <spacetuna40@gmail.com>
2026-02-12 14:38:06 -0700

    Completed unity part of the assignment

---
+++ Unity/Assets/Scripts/Caller.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Caller : MonoBehaviour
+{
+    public Receiver receiver;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // invoke method from receiver
+        receiver.OnCalled();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity/Assets/Scripts/Receiver.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Receiver : MonoBehaviour
+{
+    // This method prints 'Hello World' when called
+    public void OnCalled()
+    {
+        Debug.Log("Hello World");
+    }
+}
