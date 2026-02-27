016419f2633dd3b49bbd55324a2fcdfbdb01f5f5
SpaceTunaaa <spacetuna40@gmail.com>
2026-02-26 22:04:04 -0700

    compelted coin collect level unity

---
+++ Unity/Assets/Script/PressurePlate.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player") 
+        {
+            Debug.Log("Coin collected!");
+        }
+    }
+}
